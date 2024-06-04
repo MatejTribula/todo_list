e558b3a529dbff18bfd42bbbdfab4be8591c8265
mattveym <matej.tribula@gmail.com>
2024-06-04 20:52:56 +0200

    Minor changes in HTML

--- src/index.js
+++ src/index.js
@@ -2,17 +2,23 @@ import "./main.css"
 import controller from "./js/controller.js"
 import domController from "./js/domController.js"
 
-const zdeno = controller.createTodo('zdeno', 'default')
-controller.addTodo(zdeno)
+// const zdenoo = controller.createTodo('zdeno', 'default')
+// controller.addTodo(zdenoo)
+
+// const zdeno = controller.createProject('ladislavos')
+// controller.addProject(zdeno)
+
+// controller.removeTodo('zdeno')
 
 
 console.log(controller.getProjects())
-console.log(controller.getTodos())
+// console.log(controller.getTodos())
 // console.log(controller.projects)
 
 const body = document.body
 const projectItems = document.getElementById('projectContainer').querySelector('.project-items')
 const todoItems = document.getElementById('todoContainer').querySelector('.todo-items')
+
 
 const currentProject = document.querySelector('.current')
 
@@ -23,3 +29,8 @@ const currentProject = document.querySelector('.current')
 
 domController.showProjects(controller.getProjects(), projectItems, currentProject)
 domController.showTodos(controller.getTodos(), todoItems, controller.getProjects())
+
+// domController.projectDeleteBtn(projectItems)
+domController.changeCurrentListener()
+
+// domController.iteamCreationPopup('Project', 'projectTitle')
