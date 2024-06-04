e6c1842be62fb0e7a7f42b2a5cd0539b57adba0a
mattveym <matej.tribula@gmail.com>
2024-06-04 20:49:46 +0200

    Updated logic for creation of Projects and Todos. Created functions for deletion of Todos and Projects. Created changeCurrent func that sets new current Project

--- src/js/controller.js
+++ src/js/controller.js
@@ -4,7 +4,10 @@ import Project from "./project.js"
 const defaultProject = new Project('default')
 defaultProject.current = true
 
+
+
 class Controller {
+
 
     constructor() {
         this.todos = this.getTodos() || []
@@ -17,12 +20,43 @@ class Controller {
     }
 
     createTodo(title, project) {
-        return new Todo(title, project)
+        let match = false
+
+        const todos = this.getTodos()
+        if (todos) {
+
+            todos.forEach(todoItem => {
+                if (todoItem.title == title) {
+                    match = true
+                }
+            })
+        }
+
+
+        if (!match) {
+            return new Todo(title, project)
+        } else {
+            return null
+        }
+
+    }
+
+    removeTodo(todoTitle) {
+        const toRemoveTodo = this.todos.find(todo => todo.title == todoTitle)
+        const toRemoveIndex = this.todos.indexOf(toRemoveTodo)
+
+        if (toRemoveIndex !== -1) {
+            this.todos.splice(toRemoveIndex, 1)
+        }
+
+        this.saveTodos()
     }
 
     addTodo(todo) {
-        this.todos.push(todo)
-        this.saveTodos()
+        if (todo) {
+            this.todos.push(todo)
+            this.saveTodos()
+        }
     }
 
 
@@ -34,14 +68,42 @@ class Controller {
         return JSON.parse(localStorage.getItem('todos')) || []
     }
 
+    createProject(title) {
+        let match = false
+        const projects = this.getProjects()
+        if (projects) {
 
-    createProject(type) {
-        return new Project(type)
+            projects.forEach(projectItem => {
+                if (projectItem.type == title) {
+                    match = true
+                }
+            })
+        }
+
+        if (!match) {
+            return new Project(title)
+        } else {
+            return null
+        }
+
+    }
+
+    removeProject(projectTitle) {
+        const toRemoveProject = this.projects.find(project => project.title == projectTitle)
+        const toRemoveIndex = this.todos.indexOf(toRemoveProject)
+
+        if (toRemoveIndex !== -1 && projectTitle != 'default') {
+            this.project.splice(toRemoveIndex, 1)
+        }
+
+        this.saveProjects()
     }
 
     addProject(project) {
-        this.projects.push(project)
-        this.saveProjects()
+        if (project) {
+            this.projects.push(project)
+            this.saveProjects()
+        }
     }
 
     saveProjects() {
@@ -50,6 +112,16 @@ class Controller {
 
     getProjects() {
         return JSON.parse(localStorage.getItem('projects')) || []
+    }
+
+    changeCurrent(newCurrentName) {
+
+        const currentProject = this.projects.find(project => project.current === true)
+        currentProject.current = false
+
+        const newCurrentProject = this.projects.find(project => project.type === newCurrentName)
+
+        newCurrentProject.current = true
     }
 
 }
