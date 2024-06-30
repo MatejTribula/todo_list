31e223fbce45dcb8c32c97dd0432771a3141339a
mattveym <matej.tribula@gmail.com>
2024-06-30 14:46:24 +0200

    clean up

--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
 import "./main.css"
 import domController from "./js/domController.js"
 
-// const zdenoo = controller.createTodo('zdeno', 'default')
-// controller.addTodo(zdenoo)
-
-// const zdeno = controller.createProject('sport')
-// controller.addProject(zdeno)
-// const zdeno = controller.createProject('hana')
-// controller.addProject(zdeno)
-
-// console.log(controller.getProjects())
-// console.log(controller.getTodos())
-
-// console.log(controller.getTodos())
-
 domController.showProjects()
 domController.showTodos()
 
